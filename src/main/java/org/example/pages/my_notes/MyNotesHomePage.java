e8591d4ff12cf799a407b20a3832c727ed6f2de9
dzmitryviachaslavau <6870064@gmail.com>
2026-02-04 15:42:58 +0100

    Adding locators for the feature 'Create new note'

--- src/main/java/org/example/pages/my_notes/MyNotesHomePage.java
+++ src/main/java/org/example/pages/my_notes/MyNotesHomePage.java
@@ -1,5 +1,6 @@
 package org.example.pages.my_notes;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import org.example.components.HeaderComponent;
 import org.example.constants.routes.UIRotes;
@@ -7,6 +8,21 @@ import org.example.pages.BasePage;
 
 public class MyNotesHomePage extends BasePage {
   public final HeaderComponent header;
+
+  private final Locator searchButton = page.getByTestId("search-btn");
+  private final Locator searchInput = page.getByTestId("search-btn");
+  private final Locator addNoteButton = page.getByTestId("add-new-note");
+  private final Locator noNotes = page.getByTestId("no-notes-message");
+  private final Locator categoryAll = page.getByTestId("category-all");
+  private final Locator categoryHome = page.getByTestId("category-home");
+  private final Locator categoryWork = page.getByTestId("category-work");
+  private final Locator categoryPersonal = page.getByTestId("category-personal");
+  private final Locator createNoteCategory = page.getByTestId("note-category");
+  private final Locator createNoteCompleted = page.getByTestId("note-completed");
+  private final Locator createNoteTitle = page.getByTestId("note-title");
+  private final Locator createNoteDescription = page.getByTestId("note-description");
+  private final Locator createNoteSubmit = page.getByTestId("note-submit");
+  private final Locator createNoteCancel = page.getByTestId("note-cancel");
 
   public MyNotesHomePage(Page page) {
     super(page);
